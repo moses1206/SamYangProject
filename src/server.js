962304899b10ecbbd10dcb9b27ade36d626f4987
unknown <mose23@naver.com>
2022-02-06 19:24:18 +0900

    Production

--- src/server.js
+++ src/server.js
@@ -4,16 +4,15 @@ import promotionRouter from './routes/promotionRoute.js'
 import userRouter from './routes/userRoute.js'
 import commentRouter from './routes/commentRoute.js'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
 import generateFakeData from '../faker2.js'
-
-dotenv.config()
 
 import colors from 'colors'
 
 const server = async () => {
   try {
-    const MONGO_URI = process.env.MONGO_URI
+    const { MONGO_URI, NODE_ENV } = process.env
+    if (!MONGO_URI) throw new Error('MONGO_URI is required!!')
+
     await mongoose.connect(MONGO_URI)
 
     // mongoose.set('debug', true)
@@ -28,9 +27,7 @@ const server = async () => {
 
     const PORT = process.env.PORT || 5000
     app.listen(PORT, async () => {
-      console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${5000}`.rainbow
-      )
+      console.log(`Server running in ${NODE_ENV} mode on port ${5000}`.rainbow)
       // console.time('insert time: ')
       // for (let i = 0; i < 20; i++) {
       // await generateFakeData(10, 10, 10)
